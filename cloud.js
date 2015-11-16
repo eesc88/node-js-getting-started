ce7750709247094c4039d945c5b70a1e8038a146
eesc88 <cceecc@sina.cn>
2015-11-16 21:51:06 +0800

    冲突整理完成

--- cloud.js
+++ cloud.js
@@ -3,15 +3,7 @@ var AV = require('leanengine');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
-});
 
-AV.Cloud.define('Todo', function(request, response) {
-  response.success('Todo world!');
-});
-
-=======
 AV.Cloud.define('hello', function (request, response) {
     response.success('Hello world!');
 });
