316f72d4550baa64b0499bb9de9c32caab2b1808
eesc88 <cceecc@sina.cn>
2015-11-16 09:34:18 +0800

    云代码修改

--- cloud.js
+++ cloud.js
@@ -13,44 +13,44 @@ AV.Cloud.define('Regist', function (request, response) {
     response.success('Hello world!-->Regist');
 });
 
-//AV.Cloud.afterSave('Todo', function (request) {
-//    //输出信息请到「应用控制台 > 存储 > 云引擎 > 日志」中查看
-//    console.log(request.object);
-//    request.object.set('from', 'LeanCloud');
-//    request.object.save(null, {
-//        success: function (user) {
-//            console.log('ok!');
-//        }, error: function (user, error) {
-//            console.log('error', error);
-//        }
-//    });
-//});
+AV.Cloud.afterSave('Todo', function (request) {
+    //输出信息请到「应用控制台 > 存储 > 云引擎 > 日志」中查看
+    console.log(request.object);
+    request.object.set('from', 'LeanCloud');
+    request.object.save(null, {
+        success: function (user) {
+            console.log('ok!');
+        }, error: function (user, error) {
+            console.log('error', error);
+        }
+    });
+});
 
-//AV.Cloud.beforeUpdate('Todo', function (request, response) {
-//    // 如果 comment 字段被修改了，检查该字段的长度
-//    if (request.object.updatedKeys.indexOf('comment') != -1) {
-//        if (request.object.get('comment').length <= 140) {
-//            response.success();
-//        } else {
-//            // 拒绝过长的修改
-//            response.error('commit 长度不得超过 140 字符');
-//        }
-//    } else {
-//        response.success();
-//    }
-//});
+AV.Cloud.beforeUpdate('Todo', function (request, response) {
+    // 如果 comment 字段被修改了，检查该字段的长度
+    if (request.object.updatedKeys.indexOf('comment') != -1) {
+        if (request.object.get('comment').length <= 140) {
+            response.success();
+        } else {
+            // 拒绝过长的修改
+            response.error('commit 长度不得超过 140 字符');
+        }
+    } else {
+        response.success();
+    }
+});
 
-//AV.Cloud.beforeUpdate('_User', function (request, response) {
-//    // 如果 comment 字段被修改了，检查该字段的长度
-//    if (request.object.updatedKeys.indexOf('comment') != -1) {
-//        if (request.object.get('comment').length <= 140) {
-//            response.success();
-//        } else {
-//            // 拒绝过长的修改
-//            response.error('commit 长度不得超过 140 字符');
-//        }
-//    } else {
-//        response.success();
-//    }
-//});
+AV.Cloud.beforeUpdate('_User', function (request, response) {
+    // 如果 comment 字段被修改了，检查该字段的长度
+    if (request.object.updatedKeys.indexOf('comment') != -1) {
+        if (request.object.get('comment').length <= 140) {
+            response.success();
+        } else {
+            // 拒绝过长的修改
+            response.error('commit 长度不得超过 140 字符');
+        }
+    } else {
+        response.success();
+    }
+});
 module.exports = AV.Cloud;
